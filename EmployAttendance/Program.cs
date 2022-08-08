b3464f533c2431fcc2d439ffa963d5ec9d41e404
Aj007ar <aj007.ar@gmail.com>
2022-08-08 13:07:14 +0530

    UC1 file added

---
+++ EmployAttendance/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployAttendance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***************EMPLOYEE ATTENDANCE PROGRAM**********************");
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("\nEmployee is Present");
+            }
+            else
+            {
+                Console.WriteLine("\nEmployee is Absent");
+            }
+        }
+    }
+}
