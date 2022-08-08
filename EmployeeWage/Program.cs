45ec9f443d767fcf8957104dc384f0ca5faf8f71
Aj007ar <aj007.ar@gmail.com>
2022-08-08 13:19:18 +0530

    UC2 file added

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n***************Calculate Daily Wage for Employee***************");
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                int empRatePerHr = 20;
+                int empHour = 8;
+                int salary = (empRatePerHr * empHour);
+                Console.WriteLine("\nEmployee present for full day then salary is: " + salary);
+            }
+            else
+            {
+                int salary = 0;
+                Console.WriteLine("\nEmployee is Absent then salary is:" + salary);
+            }
+        }
+    }
+}
