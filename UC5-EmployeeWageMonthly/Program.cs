141544e76ada8275bd67f5335519512847923182
Aj007ar <aj007.ar@gmail.com>
2022-08-08 15:56:20 +0530

    UC5 file added

---
+++ UC5-EmployeeWageMonthly/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace UC5_EmployeeWageMonthly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n******************Calculate Salary for Month******************");
+            const int Part_Time = 2;
+            const int Full_Time = 1;
+            const int empRatePerHr = 20;
+            const int numOfWorkDay = 20;
+
+            int empHr = 0;
+            int totalwage = 0;
+
+            for (int i = 1; i <= numOfWorkDay; i++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case Full_Time:
+                        empHr = 8;
+                        break;
+                    case Part_Time:
+                        empHr = 4;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+
+                }
+                int salary = empHr * empRatePerHr;
+                totalwage += salary;
+            }
+            Console.WriteLine("\nEmployee wage for month is:" + totalwage);
+        }
+    }
+    
+}
